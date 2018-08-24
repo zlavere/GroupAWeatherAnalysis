f74896a94e7bef1c1c456371d0c8a8cd085cf237
Zachary Lavere <zlavere@westga.edu>
2018-08-24 17:08:01 -0400

    Started DayTemperature class.

---
+++ WeatherDataAnalysisA1/WeatherDataAnalysis/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeatherDataAnalysis
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        #region Data members
+
+        /// <summary>
+        ///     The application height
+        /// </summary>
+        public const int ApplicationHeight = 355;
+
+        /// <summary>
+        ///     The application width
+        /// </summary>
+        public const int ApplicationWidth = 625;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MainPage" /> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchViewSize = new Size {Width = ApplicationWidth, Height = ApplicationHeight};
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(ApplicationWidth, ApplicationHeight));
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void loadFile_Click(object sender, RoutedEventArgs e)
+        {
+            this.summaryTextBox.Text = "Load file was invoked.";
+        }
+
+        #endregion
+    }
+}
---
+++ WeatherDataAnalysisA1/WeatherDataAnalysis/Model/DayTemperature.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDataAnalysis.Model
+{
+    /// <summary>
+    /// High and low teperature data for a specific day
+    /// </summary>
+    public class DayTemperature
+    {
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the high temperature.
+        /// </summary>
+        /// <value>
+        /// The high temperature.
+        /// </value>
+        public int HighTemp { get; set; }
+        /// <summary>
+        /// Gets or sets the low temperature.
+        /// </summary>
+        /// <value>
+        /// The low temporary.
+        /// </value>
+        public int LowTemp { get; set; }
+        /// <summary>
+        /// Gets or sets the date.
+        /// </summary>
+        /// <value>
+        /// The date.
+        /// </value>
+        public DateTime Date { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="lowTemp"></param>
+        /// <param name="highTemp"></param>
+        public DayTemperature(DateTime date, int lowTemp, int highTemp)
+        {
+            this.Date = date;
+            this.LowTemp = lowTemp;
+            this.HighTemp = highTemp;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Gets the difference between high low.
+        /// </summary>
+        /// <returns>the difference between high low temperature</returns>
+        public int GetDifferenceHighLow()
+        {
+            return this.HighTemp - this.LowTemp;
+        }
+
+    }
+}
