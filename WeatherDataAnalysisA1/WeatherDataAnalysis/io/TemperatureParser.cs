7e0c0c31d70c901a6aac146b28b2914a428c46c8
Zachary LaVere <zlavere@westga.edu>
2018-08-27 07:45:54 -0400

    added parser for reading temperature data

---
+++ WeatherDataAnalysisA1/WeatherDataAnalysis/io/TemperatureParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using WeatherDataAnalysis.Model;
+
+namespace WeatherDataAnalysis.io
+{
+    /// <summary>
+    /// Parser for CSV set of Temperature Data where format is date, high, low.
+    /// </summary>
+    public class TemperatureParser
+    {
+        private List<string> TemperatureData { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TemperatureParser"/> class.
+        /// </summary>
+        /// <param name="temperatureData">The temperature data.</param>
+        public TemperatureParser(List<string> temperatureData)
+        {
+            this.TemperatureData = temperatureData;
+        }
+
+        public List<DayTemperature> GetAllHighLowTempData()
+        {
+            var data = new List<DayTemperature>();
+
+            var formats = new string[] { "MM/dd/yyyy", "M/d/yyyy","M/dd/yyyy", "MM/d/yyyy"};
+
+            foreach (var currentDateData in this.TemperatureData)
+            {
+                var splitData = currentDateData.Split(',');
+                var date = DateTime.ParseExact(splitData[0], "M/d/yyyy", CultureInfo.InvariantCulture);
+                var highTemp = int.Parse(splitData[1]);
+                var lowTemp = int.Parse(splitData[2]);
+           
+                data.Add(new DayTemperature(date, highTemp, lowTemp));
+            }
+            return data;
+        }
+
+        
+    }
+}
---
+++ WeatherDataAnalysisA1/WeatherDataAnalysis/io/WeatherDataInput.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherDataAnalysis.io
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WeatherDataInput
+    {
+        private readonly StreamReader inputStream;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeatherDataInput"/> class.
+        /// </summary>
+        /// <param name="filePath">The file path.</param>
+        public WeatherDataInput(string filePath)
+        {
+            this.inputStream = new StreamReader(filePath);   
+        }
+
+        /// <summary>
+        /// Gets the data.
+        /// </summary>
+        /// <value>
+        /// The data.
+        /// </value>
+        public List<string> Data
+        {
+            get
+            {
+                var data = new List<string>();
+
+                while (!this.inputStream.EndOfStream)
+                {
+                    var value = this.inputStream.ReadLine();
+                    data.Add(value);
+                }
+
+                return data;
+            }
+        }
+    }
+}
