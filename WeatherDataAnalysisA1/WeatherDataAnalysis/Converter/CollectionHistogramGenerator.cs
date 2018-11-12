d20a999cf7f42e401eecf98c0a3a2b835bcf1965
Zachary LaVere <zlavere@westga.edu>
2018-11-12 07:25:58 -0500

    Histogram Output working

---
+++ WeatherDataAnalysisA1/WeatherDataAnalysis/Converter/CollectionHistogramGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using WeatherDataAnalysis.Format;
+using WeatherDataAnalysis.Model;
+
+namespace WeatherDataAnalysis.Converter
+{
+    public class CollectionHistogramGenerator :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var collection = (List<WeatherInfo>) value;
+            var histogramGenerator = new WeatherHistogramGenerator();
+
+            return $"{histogramGenerator.CreateTempHistogram(collection)}{Environment.NewLine}" + 
+                   $"{histogramGenerator.CreatePrecipitationHistogram(collection)}";
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
